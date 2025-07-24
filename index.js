a75586f8be812877916cf336e5785bbf49cbd41a
TYLER❤️‍🩹 <145607021+RocK-EviL@users.noreply.github.com>
2025-07-24 10:37:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const axios = require('axios');
 
 // Initialize Express app
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Bot configuration
 let botConfig = {
